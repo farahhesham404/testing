f5f623678bad134c89b41f7ede3a79754b3564e1
farahhesham404 <farahheshamm404@gmail.com>
2026-05-16 14:58:48 +0300

    fixed user to custoemr2 due to customer1 being blocked

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-pr
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
+  allowCypressEnv: false,
   e2e: {
     baseUrl: "https://practicesoftwaretesting.com",
     specPattern: "cypress/e2e/features/*.feature",
